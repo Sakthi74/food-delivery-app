3f22089f6344e401be7fdd69d8c4dbd9ed30a0cc
Johnson3032000 <johnson3032000@gmail.com>
2026-06-29 16:30:01 +0530

    done payments and restaurantcarousel

--- Food_App/src/App.tsx
+++ Food_App/src/App.tsx
@@ -8,10 +8,10 @@ import Payment from "./component/Payment";
 function App() {
   return (
     <>
-      {/* <Allcategories /> */}
-      {/* <Allcategories1/>  */}
+      {/* <Allcategories />
+      <Allcategories1/> 
       <RestaurantCarousel />
-      {/* <Payment/> */}
+      <Payment/> */}
     </>
   );
 }
